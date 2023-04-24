9ada39216d29228fcb2b8466ae6b094275d0a415
Mamadou Samba DIALLO <sdiallo@MacBook-Pro-de-ENEANCE-2.local>
2023-04-24 18:23:19 +0200

    react todo context hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalState";
+
+import Heading from "./components/Heading";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <div className="h-screen text-white text-center p-10">
+        <div className="container mx-auto h-full">
+          <Heading />
+          <Routes>
+            <Route path="/" element={<TaskList />} />
+            <Route path="/add" element={<TaskForm />} />
+            <Route path="/edit/:id" element={<TaskForm />} />
+          </Routes>
+        </div>
+      </div>
+    </GlobalProvider>
   );
 }
 
